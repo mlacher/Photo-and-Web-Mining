2941853af1da0932808f5bff3cb3728488b566cc
mlacher <mlacher@live.de>
2018-07-13 17:54:51 +0200

    trasformation to HSV

--- AnalysePic.R
+++ AnalysePic.R
@@ -24,12 +24,14 @@ Path <- "C:/Users/maximilian.lacher/Documents/GitHub/Photo-and-Web-Mining"
 i=1
 #img <- readJPEG("E:/Users/lacher/Documents/GitHub/Photo-and-Web-Mining/sunset.jpg")
 Cluster_result  <- data.frame(File=character()) 
-img_h <- array(1,dim = c(y_Size,x_Size,1))
+img_h <- array(1,dim = c(y_Size,x_Size,3))
 
 
 while (i<= y_Size){
 img_hsv<-t(rgb2hsv(img[i,,1],img[i,,2],img[i,,3],1))
-img_h[i]<- img_hsv[,1]
+img_h[i,,1]<- img_hsv[,1]
+img_h[i,,2]<- img_hsv[,2]
+img_h[i,,3]<- img_hsv[,3]
 #img_s<-img_hsv[2,]
 #img_v<-img_hsv[3,]
   i = i +1;
